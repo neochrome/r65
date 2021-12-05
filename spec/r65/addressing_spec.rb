b71abd369b3ac7257633ee589dbd2f4b80845a17
neochrome <johan@stenqvist.net>
2021-12-05 17:54:57 +0100

    add more addressing specs

--- spec/r65/addressing_spec.rb
+++ spec/r65/addressing_spec.rb
@@ -1,6 +1,17 @@
 require_relative "../../lib/r65"
 
 describe R65::Addressing do
+  def a_label
+    R65::Addressing::LabelExpression.new :label
+  end
+
+  def a_label_resolved_to (address)
+    a_label.resolve! { address}
+  end
+
+  def address_of (value)
+    R65::Addressing::ConstantExpression.new 123
+  end
 
   describe R65::Addressing::Expression do
 
@@ -16,9 +27,23 @@ describe R65::Addressing do
 
       example "other expressions" do
         other = R65::Addressing::Expression.from 456
-        expect(R65::Addressing::Expression.from(other)).to be other
+        expect(R65::Addressing::Expression.from(other)).to eq other
       end
 
+    end
+
+  end
+
+  describe "labels" do
+    it "resolves to an address" do
+      label = a_label_resolved_to 123
+
+      expect(label.value.to_s).to eq "123"
+    end
+
+    it "are nil when unresolved" do
+      label = a_label
+      expect(label.value).to eq nil
     end
   end
 
@@ -34,20 +59,20 @@ describe R65::Addressing do
           456
         end
 
-        expect(val.pending?).to be false
-        expect(lbl.pending?).to be true
-        expect(exp.pending?).to be true
+        expect(val.pending?).to eq false
+        expect(lbl.pending?).to eq true
+        expect(exp.pending?).to eq true
 
-        expect(val.value).to be 123
-        expect(lbl.value).to be nil
-        expect(exp.value).to be nil
+        expect(val.value).to eq 123
+        expect(lbl.value).to eq nil
+        expect(exp.value).to eq nil
 
         exp.resolve! &lookup
 
-        expect(lbl.pending?).to be false
-        expect(exp.pending?).to be false
-        expect(lbl.value).to be 456
-        expect(exp.value).to be 579
+        expect(lbl.pending?).to eq false
+        expect(exp.pending?).to eq false
+        expect(lbl.value).to eq 456
+        expect(exp.value).to eq 579
       end
 
     end
