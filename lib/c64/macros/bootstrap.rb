329f5725db4bfd3e1b9e645e272748ca39252253
neochrome <johan@stenqvist.net>
2021-12-05 17:56:23 +0100

    c64 bootstrap macro must only be used with a block

--- lib/c64/macros/bootstrap.rb
+++ lib/c64/macros/bootstrap.rb
@@ -1,19 +1,18 @@
 module C64
   module Macros
 
-      Bootstrap = proc do |start: nil, block: nil|
-        basic = DefaultBasic::Start
-        raise ArgumentError, "Can't supply both start address and block at the same time." if start and block
-        start = basic + 13 unless start
-        pc! basic                      # [0] start of basic program
-        word 0x080c                    # [2] next basic line
-        word 0x000a                    # [2] basic line number 10
-        byte 0x9e                      # [1] sys
-        byte start.to_s.to_scr         # [4-5] address in decimal screencodes
-        byte 0x00                      # [1] basic <eol>
-        word 0x0000                    # [2] end of basic program
-        byte 0x00 if start < 10000     # [1-0] padding
-        call block if block.is_a? Proc # total: 13 bytes
+      Bootstrap = proc do |block:|
+        raise ArgumentError, "Must supply a block." unless block.is_a? Proc
+        start = DefaultBasic::Start + 13 # start 13 bytes after basic
+        pc! DefaultBasic::Start          # [0] start of basic program
+        word 0x080c                      # [2] next basic line
+        word 0x000a                      # [2] basic line number 10
+        byte 0x9e                        # [1] sys
+        byte start.to_s.to_scr           # [4-5] address in decimal screencodes
+        byte 0x00                        # [1] basic <eol>
+        word 0x0000                      # [2] end of basic program
+        byte 0x00 if start < 10000       # [1-0] padding
+        call block                       # total: 13 bytes
       end
 
   end
