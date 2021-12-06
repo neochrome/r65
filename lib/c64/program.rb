fbcafe02034f13bab082aee2c59d0bc1575e4af8
neochrome <johan@stenqvist.net>
2021-12-06 11:55:20 +0100

    add support to launch vice with symbols loaded

--- lib/c64/program.rb
+++ lib/c64/program.rb
@@ -4,10 +4,23 @@ module C64
 
   class Program < R65::Program
 
-    def write_and_run (filename = nil)
-      filename = File.basename(Kernel.caller_locations.first.absolute_path, ".rb") + ".prg" unless filename
-      write filename
-      exec "x64", "-autostartprgmode", "1", filename
+    def write_and_run (filename = nil, debug: false)
+      filename = write filename
+      if debug
+        symbols = as_symbols.map do |sym|
+          label = sym[:label].gsub(":",".")
+          "al %04x %s" % [sym[:address], label]
+        end
+        symbols_file = filename + ".sym"
+        File.open symbols_file, "wb" do |file|
+          file.print symbols.join("\n")
+        end
+        puts "#{symbols.size} symbols written to #{symbols_file}"
+        exec "x64", "-moncommands", symbols_file, "-autostartprgmode", "1", filename
+      else
+        exec "x64", "-autostartprgmode", "1", filename
+      end
+
     end
 
   end
